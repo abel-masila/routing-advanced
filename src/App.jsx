f25783704b551341a932a031f89b00f6890aa044
Abel Masila <abel@x2ai.com>
2023-03-01 03:07:40 +0300

    added login flow

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { logoutAction } from "./actions/logout";
 import Main, { mainLoader } from "./layouts/Main";
 
 //Routes
-import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
 const router = createBrowserRouter([
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Dashboard />,
         loader: dashboardLoader,
+        action: dashboardAction,
         errorElement: <Error />,
       },
       {
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,23 +1,30 @@
 import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+import Intro from "../components/Intro";
 import { fetchData } from "../helpers";
 
 //Loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-
   return {
     userName,
   };
 }
+//action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
+  } catch (error) {
+    throw new Error("Error creating your account.");
+  }
+}
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
-  return (
-    <div>
-      <h3>Dashboard</h3>
-      {userName}
-    </div>
-  );
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 
 export default Dashboard;
